c04d26bf208ab08be331601152f416129a7fb927
CrispenGari <Crispengari@gmail.com>
2021-06-27 14:08:12 +0200

    react-native-elements

--- react-native-elements/App.js
+++ react-native-elements/App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Tab, TabView, Text } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Tab value={index} onChange={setIndex}>
+        <Tab.Item title="recent" />
+        <Tab.Item title="favorite" />
+        <Tab.Item title="cart" />
+      </Tab>
+
+      <TabView value={index} onChange={setIndex}>
+        <TabView.Item style={{ backgroundColor: "red", width: "100%" }}>
+          <Text h1>Recent</Text>
+        </TabView.Item>
+        <TabView.Item style={{ backgroundColor: "blue", width: "100%" }}>
+          <Text h1>Favorite</Text>
+        </TabView.Item>
+        <TabView.Item style={{ backgroundColor: "green", width: "100%" }}>
+          <Text h1>Cart</Text>
+        </TabView.Item>
+      </TabView>
     </View>
   );
 }
@@ -14,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ react-native-elements/README.md
@@ -0,0 +1,40 @@
+### Beautiful Components
+
+- [Docs](https://reactnativeelements.com/docs/)
+
+1. Avatar
+2. Badge
+3. Bottom Sheet, Amazing UI
+4. Button
+5. ButtonGroup
+6. Card
+7. Checkbox
+8. Chip
+9. Divider
+10. Floating Action Button
+11. Header
+12. Icon
+13. Image
+14. Input
+15. Linear Progress
+16. ListItem
+17. OverLay
+18. Pricing
+19. Ratting
+20. SearchBar
+21. Slider
+22. SocialIcon
+23. Speed Dial - Amazing UI
+24. Switch
+25. Tabs - Beautiful UI
+26. Text
+27. Tile - Amazing UI
+28. Tooltip
+
+### React Native Universe components
+
+```
+npm i react-native-elements-universe
+```
+
+1. CircularSlider - Amazing
