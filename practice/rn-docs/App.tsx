6f4385209edb92fc3d021a2a492904ba5279a6b1
CrispenGari <Crispengari@gmail.com>
2021-07-21 20:46:24 +0200

    read-the-docs-d1

--- practice/rn-docs/App.tsx
+++ practice/rn-docs/App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { StyleSheet, Platform, View, Button, Linking } from "react-native";
 
-export default function App() {
+const App = () => {
+  console.log(Platform.OS, Platform.Version, Platform.isTV);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="openGoogle" />
     </View>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
