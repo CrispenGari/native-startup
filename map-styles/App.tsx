7222374eb84b5e1ee220b42e0d0fd3bce97979e6
CrispenGari <Crispengari@gmail.com>
2021-08-06 11:22:53 +0200

    map-styles

---
+++ map-styles/App.tsx
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+
+import * as Location from "expo-location";
+import Header from "./components/Header";
+import mapStyles from "./utils/mapstyles";
+const { width, height } = Dimensions.get("screen");
+
+interface LocationI {
+  latitude: number;
+  longitude: number;
+}
+type LocationType = LocationI;
+
+const App: React.FC = () => {
+  const [location, setLocation] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [currentLocation, setCurrentLocation] = useState<LocationType | any>();
+
+  const [selectedLocation, setSelectedLocation] = useState<
+    LocationType | any
+  >();
+
+  const [selectedReversedLocation, setSelectedReversedLocation] =
+    useState<any>();
+  const [currentReversedLocation, setCurrentReversedLocation] = useState<any>();
+
+  React.useEffect(() => {
+    let mounted: boolean = true;
+    (async () => {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (granted && mounted) {
+        const { coords } = await Location.getCurrentPositionAsync();
+        setLocation(coords);
+        const { latitude, longitude } = coords;
+        setCurrentLocation({
+          latitude,
+          longitude,
+        });
+        setLoading(false);
+      }
+    })();
+    return () => {
+      mounted = false;
+    };
+  }, []);
+  React.useEffect(() => {
+    let mounted: boolean = true;
+    (async () => {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (granted && mounted && currentLocation) {
+        const reversed = await Location.reverseGeocodeAsync(currentLocation);
+        setCurrentReversedLocation(reversed[0]);
+      }
+    })();
+    return () => {
+      mounted = false;
+    };
+  }, [currentLocation]);
+  React.useEffect(() => {
+    let mounted: boolean = true;
+    (async () => {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (granted && mounted && selectedLocation) {
+        const reversed = await Location.reverseGeocodeAsync(selectedLocation);
+        setSelectedReversedLocation(reversed[0]);
+      }
+    })();
+    return () => {
+      mounted = false;
+    };
+  }, [selectedLocation]);
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator size="large" color="lightseagreen" />
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <Header cityName={selectedReversedLocation?.name} />
+      <MapView
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 1,
+          longitudeDelta: 1,
+        }}
+        style={{
+          width,
+          height,
+        }}
+        onPress={(event) => {
+          setSelectedLocation(event.nativeEvent.coordinate);
+        }}
+        customMapStyle={mapStyles}
+      >
+        <Marker
+          coordinate={{
+            latitude: currentLocation?.latitude,
+            longitude: currentLocation.longitude,
+          }}
+        >
+          <Callout>
+            <Text>{currentReversedLocation?.name}</Text>
+          </Callout>
+        </Marker>
+        {Boolean(selectedLocation) ? (
+          <Marker
+            draggable
+            onDragEnd={(event) => {
+              setSelectedLocation(event.nativeEvent.coordinate);
+            }}
+            coordinate={{
+              latitude: selectedLocation?.latitude,
+              longitude: selectedLocation?.longitude,
+            }}
+          >
+            <Callout>
+              <Text>{selectedReversedLocation?.name}</Text>
+            </Callout>
+          </Marker>
+        ) : null}
+      </MapView>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ map-styles/README.md
@@ -0,0 +1,27 @@
+### Customizing A map.
+
+In this simple application we build a simple map view with custom styles which were found at [mapstyle.withgoogle](https://mapstyle.withgoogle.com/) to customize our map to look as follows:
+
+### Demo
+
+![img]()
+
+### Installations
+
+1. `react-native-maps`
+
+```
+expo install react-native-maps
+```
+
+2. `expo-location`
+
+```
+expo install expo-location
+```
+
+### Docs and Resources
+
+- [mapstyle.withgoogle](https://mapstyle.withgoogle.com/)
+- [expo-location](https://docs.expo.dev/versions/latest/sdk/location/#locationreversegeocodeasynclocation)
+- [react-native-maps](https://github.com/react-native-maps/react-native-maps)
