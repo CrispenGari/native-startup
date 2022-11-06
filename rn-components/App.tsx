0cb6b3ca999c7d53ff62d77aaf8667cf201a90b4
CrispenGari <Crispengari@gmail.com>
2022-11-06 08:18:46 +0200

    eee

--- rn-components/App.tsx
+++ rn-components/App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { View, Button } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { ImageBackground, Text, Image } from "react-native";
+
 const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button title="Open Modal" />
-    </View>
+    <ImageBackground
+      source={{
+        uri: Image.resolveAssetSource(require("./assets/image.jpg")).uri,
+      }}
+    >
+      <Text>Hello React Native</Text>
+    </ImageBackground>
   );
 };
 export default () => <App />;
